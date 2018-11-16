77e5b3505bd94e298f4056c1ec979e17ec9695d7
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2018-11-16 14:34:02 +0100

    add keyloading, add internet connection test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Archlinux-Install-Script
+
+
+
+1. Run loadkeys de-latin1
+2. Connect to the internet via wifi-menu or so2. Connect to the internet via wifi-menu or so
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+DRIVE="dev/sda"
+KEY_LAYOUT="de-latin1"
+
+
+
+# 1 Loadkeys
+echo "Set Keylayout"
+loadkeys $KEY_LAYOUT
+
+#2 Check internet connection
+echo "Checking internet connection"
+ping google.de -c 1
+
+if [[ $? -eq 0 ]]
+   echo "No internet connection found. Please connect and rerun this script"
+   exit
+fi
